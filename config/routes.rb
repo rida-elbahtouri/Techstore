d087e8599f97a00df9fd8027124259d65d3ffdef
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-10-03 08:08:06 -0700

    add customer detail controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "products#index"
   resources :products
+  resources :cus_details
+
+
+  # get '/details/both',to: "userdetails#new_both"
+  # get '/details/customer',to: "userdetails#new_customer"
+  # get '/details/seller',to: "userdetails#new_seller"
+
 end
