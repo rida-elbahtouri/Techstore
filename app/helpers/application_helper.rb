b6bff4d95fbf212b517f1c26fe19f6d95b824b2f
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-11-14 18:06:20 +0100

    add new prices after promotion to products card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,5 +5,15 @@ module ApplicationHelper
         else
             content_tag(:div, 'No photo')
         end
-end
+    end
+
+    def promotion(product)
+        if product.promotion != 0
+            @newprice = product.price - (product.price * product.promotion)/100
+            @prod = product
+            render "price"
+        else
+            content_tag(:span, "#{product.price} $")
+        end
+    end
 end
