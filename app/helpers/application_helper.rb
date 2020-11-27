08b024df094407eeef519c07be906a69f927c609
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-11-27 16:07:24 +0100

    add seller products to his profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,35 +1,39 @@
 module ApplicationHelper
-   def image(product)
-        if product.photo
-            image_tag(product.photo, class: 'img')
-        else
-            content_tag(:div, 'No photo')
-        end
+  def image(product)
+    if product.photo
+      image_tag(product.photo, class: 'img')
+    else
+      content_tag(:div, 'No photo')
     end
+  end
 
-    def promotion(product)
-        if product.promotion != 0
-            @newprice = product.price - (product.price * product.promotion)/100
-            @prod = product
-            render "price"
-        else
-            content_tag(:span, "#{product.price} $")
-        end
+  def promotion(product)
+    if product.promotion != 0
+      @newprice = product.price - (product.price * product.promotion) / 100
+      @prod = product
+      render 'products/price'
+    else
+      content_tag(:span, "#{product.price} $")
     end
+  end
 
-    def addorgotobasket(product)
-        @product = product
-        basketproducts = current_user.baskets.pluck(:product_id)
+  def addorgotobasket(product)
+    @product = product
+    basketproducts = current_user.baskets.pluck(:product_id)
 
-        if basketproducts.include?(product.id)
-            link_to "See Basket", baskets_path,class:"visit-basket"
-        else
-            render "products/addtocardbutton"
-        end
+    if basketproducts.include?(product.id)
+      link_to 'See Basket', baskets_path, class: 'visit-basket'
+    else
+      render 'products/addtocardbutton'
     end
+  end
 
-    def results(products)
-        @products = products
-        render "products/results"
-    end
+  def results(products)
+    @products = products
+    render 'products/results'
+  end
+
+  def sellerprofile(user)
+    render 'users/seller' if user.service == 'Both'
+  end
 end
