c6336bc1ca724d4b857d81bfdd1434ecec391bd5
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-11-30 16:29:36 +0100

    add a button to remove promotion

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,71 +1,74 @@
 class ProductsController < ApplicationController
-    before_action :authenticate_user!
-    def index
-        @mostselles = Product.all.mosts.first
-        @hotselles = Product.all.mosts
-        @hurryup = Product.all.hurryup
-        @products = Product.all.limit(5)
-    end
+  before_action :authenticate_user!
+  def index
+    @mostselles = Product.all.mosts.first
+    @hotselles = Product.all.mosts
+    @hurryup = Product.all.hurryup
+    @products = Product.all.limit(5)
+  end
 
-    def show
-        @product = Product.find(params[:id])
-        @comments = @product.comments.includes(:customer)
-    end
-    def new
-        @product = Product.new
-    end
+  def show
+    @product = Product.find(params[:id])
+    @comments = @product.comments.includes(:customer)
+  end
 
-    def create
-        @product = current_user.goods.new(product_params)
-        if @product.save
-            flash[:notice] = "your product has been created succesfully"
-            redirect_to products_path
-        else
-            flash[:alert] = "we couldn't create the product"
-            render :new
-        end 
-    end
+  def new
+    @product = Product.new
+  end
 
-    def edit
-        @product = Product.find(params[:id])
+  def create
+    @product = current_user.goods.new(product_params)
+    if @product.save
+      flash[:notice] = 'your product has been created succesfully'
+      redirect_to products_path
+    else
+      flash[:alert] = "we couldn't create the product"
+      render :new
     end
+  end
 
-    def update
-        @product = current_user.goods.find(params[:id])
-        if @product.update(product_params)
-            flash[:notice] = "your product has been updated succesfully"
-            redirect_to products_path
-        else
-            flash[:alert] = "we couldn't update the product"
-            render :edit
-        end
-    end
+  def edit
+    @product = Product.find(params[:id])
+  end
 
-    def destroy
-        @product = current_user.goods.find(params[:id])
-        if @product.destroy
-            flash[:notice] = "your product has been removed succesfully"
-            redirect_to products_path
-        else
-            flash[:alert] = "we couldn't remove the product"
-            render :index
-        end
+  def update
+    @product = current_user.goods.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = 'your product has been updated succesfully'
+      redirect_to products_path
+    else
+      flash[:alert] = "we couldn't update the product"
+      render :edit
     end
-    
-    def category
-        @products = Product.all.where(categ: params[:cat]).order("selletimes DESC").limit(10)
-    end
-    def search
-        @products = Product.all.searsh("%#{params[:term]}%")
-    end
-    def productdata
-        product = Product.find(params[:id])
-        render json: product ,only: [:name,:price,:promotion]
-    end
+  end
 
-    private
-
-    def product_params
-        params.require(:product).permit(:name,:description,:price,:categ,:photo)
+  def destroy
+    @product = current_user.goods.find(params[:id])
+    if @product.destroy
+      flash[:notice] = 'your product has been removed succesfully'
+      redirect_to products_path
+    else
+      flash[:alert] = "we couldn't remove the product"
+      render :index
     end
+  end
+
+  def category
+    @products = Product.all.where(categ: params[:cat]).order('selletimes DESC').limit(10)
+  end
+
+  def search
+    @products = Product.all.searsh("%#{params[:term]}%")
+  end
+
+  def productdata
+    product = Product.find(params[:id])
+    render json: product, only: %i[name price promotion]
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :categ, :photo, :promotion)
+  end
 end
