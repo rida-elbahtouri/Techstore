0bee83c807c8b93b6829903a4621192b2775a44e
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-12-13 12:01:15 +0100

    add update rating btn to product page

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -12,4 +12,17 @@ class RatingsController < ApplicationController
       render :new
     end
   end
+
+  def update
+    stars = params[:stars][-1].to_i
+    oldRating = current_user.ratings.find(params[:id])
+    oldRating.stars = stars
+    if oldRating.save
+      flash[:notice] = 'your review  has been updated succesfully'
+      redirect_to products_path
+    else
+      flash[:alert] = "we couldn't update your rating"
+      render :new
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,4 +49,26 @@ module ApplicationHelper
   def colored(stars, avg)
     'coloredstar' if stars <= avg
   end
+
+  def showRatingbtn(product)
+    # condition to show rate btn
+    if Order.find_by(customer_id: current_user.id, product_id: product.id)
+      rating = current_user.ratings.find_by(product_id: product.id)
+      if rating
+        content_tag(:button, 'Update your Rating', class: 'rate_btn', id: 'rate_product')
+      else
+        content_tag(:button, 'Rate the Product', class: 'rate_btn', id: 'rate_product')
+      end
+    end
+  end
+
+  def newupdaterating(product)
+    # condition for update review
+    rating = current_user.ratings.find_by(product_id: product.id)
+    if rating
+      button_to  'Submit', rating_path(rating.id), params: { product_id: product.id, stars: 'ratingsubmmitvalue' }, method: :patch, class: 'remove_promotion'
+    else
+      button_to  'Submit', ratings_path, params: { product_id: product.id, stars: 'ratingsubmmitvalue' }, method: :post, class: 'remove_promotion'
+    end
+  end
 end
