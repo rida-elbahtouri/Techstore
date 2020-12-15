c0a39f9013f15babeab6ec25ed6cdf57265533f9
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-12-15 13:54:41 +0100

    fix drop down menu in phone and tap devises

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,22 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import "@fortawesome/fontawesome-free/js/all"; 
-console.log("hello rails javascript")
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+window.onload = function(){
+const categorybtn = document.getElementById('category-nav');
+const categorylist = document.getElementById('category-list');
+document.onclick = (e)=>{
+    if(e.target === categorybtn){
+        categorylist.style.display = 'block';
+    }else {
+        categorylist.style.display = 'none';
+    }
+};
+};
