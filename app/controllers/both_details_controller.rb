1bc24f9598b17f37274eecf2990111bdd79859b8
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-12-23 11:06:04 +0100

    allow user to update services adress after update user information

--- app/controllers/both_details_controller.rb
+++ app/controllers/both_details_controller.rb
@@ -1,21 +1,27 @@
 class BothDetailsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def new
+  def new; end
+
+  def create
+    if current_user.sel_detail
+      seldet = current_user.sel_detail.update(shipping_adress: params[:shipping_adress], product_type: params[:sel_detail]['product_type'])
+    else
+      seldet = current_user.create_sel_detail(shipping_adress: params[:shipping_adress], product_type: params[:sel_detail]['product_type'])
     end
 
-    def create
-        seldet = current_user.create_sel_detail(shipping_adress: params[:shipping_adress],product_type: params[:sel_detail]["product_type"])
-        cusdet = current_user.create_cus_detail( adress: params[:adress])
-        
-        if cusdet && seldet
-            flash[:notice] = "your information has been added succesfully"
-            redirect_to products_path
-        else
-            flash[:alert] = "we couldn't add your information"
-            render new
-        end   
-    end
+    cusdet = if current_user.sel_detail
+               current_user.cus_detail.update(adress: params[:adress])
+             else
+               current_user.create_cus_detail(adress: params[:adress])
+             end
 
+    if cusdet && seldet
+      flash[:notice] = 'your information has been added succesfully'
+      redirect_to products_path
+    else
+      flash[:alert] = "we couldn't add your information"
+      render new
+    end
+  end
 end
-   
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,41 +1,61 @@
 class RegistrationsController < Devise::RegistrationsController
-    def create
-        build_resource(sign_up_params)
+  def create
+    build_resource(sign_up_params)
 
-      resource.save
-      yield resource if block_given?
-      if resource.persisted?
-        if resource.active_for_authentication?
-          set_flash_message! :notice, :signed_up
-          sign_up(resource_name, resource)
-          # respond_with resource, location: after_sign_up_path_for(resource)
-          detailslink(resource.service)
-        else
-          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-          expire_data_after_sign_in!
-          respond_with resource, location: after_inactive_sign_up_path_for(resource)
-        end
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        # respond_with resource, location: after_sign_up_path_for(resource)
+        detailslink(resource.service)
       else
-        clean_up_passwords resource
-        set_minimum_password_length
-        respond_with resource
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
       end
-      
-    end
-    private
-  
-    def sign_up_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation,:service)
-    end
-  
-    def account_update_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password,:service)
-    end
-    def detailslink(link)
-      if link == "Both"
-        redirect_to new_both_detail_path
-      elsif link == "customer"
-        redirect_to new_cus_detail_path
-      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
     end
   end
+
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      set_flash_message_for_update(resource, prev_unconfirmed_email)
+      bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
+
+      # respond_with resource, location: after_update_path_for(resource)
+      detailslink(resource.service)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :service)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :service)
+  end
+
+  def detailslink(link)
+    if link == 'Both'
+      redirect_to new_both_detail_path
+    elsif link == 'customer'
+      redirect_to new_cus_detail_path
+    end
+  end
+end
