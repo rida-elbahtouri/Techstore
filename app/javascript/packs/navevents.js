7cd13ff82e7f5c94f7134fb75187409c29dcbfa3
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-12-25 09:31:07 +0100

    solve navbar issue(whene resize from mobile to web it keep the none)

--- app/javascript/packs/navevents.js
+++ app/javascript/packs/navevents.js
@@ -3,7 +3,20 @@
     const categorylist = document.getElementById('category-list');
     const humberger = document.getElementById("humberger")
     const nav = document.getElementById("navbar-content")
-    
+
+
+    function WindowSize(){
+        
+        let w = document.documentElement.clientWidth;
+        if (w > 850){
+            nav.style.display = "flex"
+        }else{
+            nav.style.display = 'none'
+        }
+    }
+        
+    // Attaching the event listener function to window's resize event
+    window.addEventListener("resize", WindowSize)
     if (window.innerWidth > 850){
     document.onclick = (e)=>{
         if(e.target === categorybtn){
