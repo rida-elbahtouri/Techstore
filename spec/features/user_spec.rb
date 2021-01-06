1d7490461a8e6ac2211db690755222a836ff8002
rida-elbahtouri <elbahtouri0123@gmail.com>
2021-01-06 11:22:56 +0100

    add test for additional information

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -9,14 +9,20 @@ RSpec.describe 'registrations_controller', type: :system do
   end
   describe 'signup/in user' do
     it 'sign up' do
-      visit new_user_registration_path
-      fill_in('user_name', with: 'user1')
-      fill_in('user_email', with: 'user1@user.com')
-      fill_in('user_password', with: 'user12')
-      fill_in('user_password_confirmation', with: 'user12')
-      find('#user_service').find(:option, 'customer and seller').select_option
-      click_button('Sign up')
+      do_sign_up('customer and seller')
       expect(page).to have_content 'Welcome! You have signed up successfully.'
+    end
+    it 'add additionl information of user' do
+      do_sign_up('customer and seller')
+      fill_in('adress', with: 'adress')
+      fill_in('shipping_adress', with: 'shipping_adress')
+      find('#sel_detail_product_type').find(:option, 'general').select_option
+      click_button('Create account')
+      expect(page).to have_content 'your information has been added succesfully'
+    end
+    it 'log in' do
+      do_login('test@test.com', '123456')
+      expect(page).to have_content 'Signed in successfully.'
     end
     it 'update user information' do
       do_login('test@test.com', '123456')
--- spec/featuresHelper/featureHelper.rb
+++ spec/featuresHelper/featureHelper.rb
@@ -5,4 +5,14 @@ module FeatureTestHelpers
     fill_in('user_password', with: password)
     click_button('Log in')
   end
+
+  def do_sign_up(service)
+    visit new_user_registration_path
+    fill_in('user_name', with: 'user1')
+    fill_in('user_email', with: 'user1@user.com')
+    fill_in('user_password', with: 'user12')
+    fill_in('user_password_confirmation', with: 'user12')
+    find('#user_service').find(:option, service).select_option
+    click_button('Sign up')
+  end
 end
